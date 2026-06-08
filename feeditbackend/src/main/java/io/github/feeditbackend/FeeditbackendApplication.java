068c643e8c9ea8a4d07ba72b95d5b7625daee823
Kryštof Absolon <krystof.absolon@gmail.com>
2026-06-08 11:38:24 +0200

    Initialized backend

---
+++ feeditbackend/src/main/java/io/github/feeditbackend/FeeditbackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.feeditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeditbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeditbackendApplication.class, args);
+	}
+
+}
---
+++ feeditbackend/src/test/java/io/github/feeditbackend/FeeditbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.feeditbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeditbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

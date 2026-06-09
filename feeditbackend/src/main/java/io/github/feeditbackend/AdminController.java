22a2474934d5f4950f8797f5b4c5fecd66c618f4
Kryštof Absolon <krystof.absolon@gmail.com>
2026-06-09 10:41:42 +0200

    Added more get request to Admin API

--- feeditbackend/src/main/java/io/github/feeditbackend/AdminController.java
+++ feeditbackend/src/main/java/io/github/feeditbackend/AdminController.java
@@ -9,14 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 import io.github.feeditbackend.objects.Feedback;
 
 
+
 @RestController
 @RequestMapping("api/admin")
 public class AdminController {
 
-    @GetMapping("/get")
-    public ArrayList<Feedback> get() {
+    @GetMapping("/getFeedback")
+    public ArrayList<Feedback> getFeedback() {
         return FeeditbackendApplication.feedbacks;
     }
+
+    @GetMapping("/getAmount")
+    public String getAmount() {
+        return String.valueOf(FeeditbackendApplication.feedbacks.size());
+    }
+
+    @GetMapping("/getTotalRating")
+    public String getTotalRating() {
+        return String.valueOf(FeeditbackendApplication.feedbacks.stream()
+            .mapToInt(f -> f.rating)
+            .sum()
+            /FeeditbackendApplication.feedbacks.size()
+        );
+    }
+    
     
     
 }

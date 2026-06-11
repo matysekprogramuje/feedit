11228f7c9b97abd105c503eccee912c523ab159b
buriak <yurii.buriak@educanet.cz>
2026-06-11 10:44:22 +0200

    login working

--- feeditbackend/src/main/java/io/github/feeditbackend/login/AuthRequest.java
+++ feeditbackend/src/main/java/io/github/feeditbackend/login/AuthRequest.java
@@ -1,8 +1,11 @@
 package io.github.feeditbackend.login;
 
+
+
 public class AuthRequest {
-    public String type;
+
     public String username;
     public String email;
     public String password;
+
 }
